9df372e5cb192b46809e2a17555d685c2b52aad8
Yolanda Martin Madrid <ymmadrid9@gmail.com>
2022-03-29 11:28:49 +0200

    images router done

--- api/src/routers/main_router.ts
+++ api/src/routers/main_router.ts
@@ -1,5 +1,36 @@
-import { FastifyPluginAsync } from 'fastify';
+import { FastifyPluginAsync, FastifyRequest } from 'fastify';
+import { Images } from '../models/Images.model';
+
+type Myrequest = FastifyRequest<{
+    Body: {title: string, img: string};
+    Params: {id: string};
+}>
 
 export const mainRouter: FastifyPluginAsync = async (app) => {
-  app.get('/', async () => ({ hello: 'world' }));
+  // Get all images
+  app.get('/', async () => {
+    const images = await Images.find().lean();
+    return images;
+  });
+  // Add a new image
+  app.post('/', async (request: Myrequest) => {
+    const { title, img } = request.body;
+    const image = new Images({ title, img });
+    await image.save();
+    return image;
+  });
+  // Delete an image
+  app.delete('/:id', async (request: Myrequest) => {
+    const { id } = request.params;
+    await Images.findByIdAndDelete(id);
+    return { status: 'delete' };
+  });
+  // Update an image
+  app.post('/:id', async (request: Myrequest) => {
+    const { id } = request.params;
+    const image = await Images.findOneAndUpdate({ _id: id }, request.body, {
+      new: true,
+    });
+    return image;
+  });
 };
